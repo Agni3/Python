bc06d2fd7026c5592b944dbdbb51dc6f91072a1e
Agni3 <32581200+Agni3@users.noreply.github.com>
2017-10-10 00:31:35 +0300

    Update guessthenumber.py

--- guessthenumber.py
+++ guessthenumber.py
@@ -1,13 +1,21 @@
 import random
 randomnum = random.randint(0,100)
 end=0
+lives=input("Number of lives:")
+while (not lives.isdigit()):
+	lives=input("Number of lives:")
 while (end==0):
 	userinput= input("Guess the number:")
 	while (not userinput.isdigit()):
 		userinput= input("Guess the number:")
 	if int(userinput)==randomnum:
 		end=1
-		print("We did it!")
+		print("We did it!") 
+	if	int(userinput)!=randomnum:
+		lives = int(lives) - 1
+	if  int(lives) == 0:
+		print("Game over :C")
+		break
 	if int(userinput)>randomnum:
 		print("Aghh...Lower!!")
 	if int(userinput)<randomnum:
