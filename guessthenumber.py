66c566050485aa522d1ef71c8a59faa55e96187b
Agni3 <32581200+Agni3@users.noreply.github.com>
2017-10-09 23:57:08 +0300

    Update guessthenumber.py

--- guessthenumber.py
+++ guessthenumber.py
@@ -2,11 +2,13 @@ import random
 randomnum = random.randint(0,100)
 end=0
 while (end==0):
-	userinput= int(input("Guess the number:"))
-	if userinput==randomnum:
+	userinput= input("Guess the number:")
+	while (not userinput.isdigit()):
+		userinput= input("Guess the number:")
+	if int(userinput)==randomnum:
 		end=1
 		print("We did it!")
-	if userinput>randomnum:
+	if int(userinput)>randomnum:
 		print("Aghh...Lower!!")
-	if userinput<randomnum:
+	if int(userinput)<randomnum:
 		print("Nopee... Higher1!1!")
